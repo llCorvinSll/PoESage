b2a634ca3b90a9425cca707fc3a55860a674a8bc
dpronin <dpronin@ivi.ru>
2017-09-07 17:03:33 +0300

    stub for screen maker

---
+++ WikiScreen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WikiScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
